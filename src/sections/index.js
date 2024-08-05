ad9a1fa0685c36a7cc574d4f00f5bf6d89b3a4fd
Rujal <rujalshrsth@gmail.com>
2024-08-06 00:39:50 +0545

    Updated Hero Card Section

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Services";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  SuperQuality,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+};
