d7589b782bc2e3a79621f08b02ecc96479b35588
Rujal <rujalshrsth@gmail.com>
2024-08-08 02:05:27 +0545

    Section Updated

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -15,13 +15,3 @@ const Services = () => {
 
 export default Services
 
-
-// <div className="grid grid-cols-3 border-gray-950 p-22">
-//   {services.map((service) => (
-  //     <div key={service.label} className="flex flex-col items-center gap-2">
-//       <img src={service.imgURL} alt={service.label} className="w-16 h-16" />
-//       <h3 className="font-palanquin font-bold text-lg">{service.label}</h3>
-//       <p className="font-montserrat text-sm text-center">{service.subtext}</p>
-//     </div>
-//   ))}
-// </div>
