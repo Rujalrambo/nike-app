ed55157aae13f37a7b543eaea9317b4cf8dfc91e
Rujal <rujalshrsth@gmail.com>
2024-08-08 01:04:37 +0545

    Special Offer section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -20,9 +20,9 @@ const SuperQuality = () => {
           Our dedication to detail and excellence ensures your satisfaction
         </p>
 
-        <div className="mt-11">
-          <Button label="View Details" />
-        </div>
+          <div className="mt-11">
+            <Button label="View Details" />
+          </div>
         </div>
 
         <div className="flex-1 flex justify-center items-center">
